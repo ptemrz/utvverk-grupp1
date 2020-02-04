e1ce7f2fef4f00771af6ab71cb851021c5be4dfd
Peter Mrazek <56875892+ptemrz@users.noreply.github.com>
2020-02-04 11:00:05 +0100

    Add maven archetype and eclipse project

---
+++ src/main/java/com/yrgo/App.java
@@ -0,0 +1,13 @@
+package com.yrgo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
