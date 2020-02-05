4cc61a4ec18706126079f31ffaa5e682ea831a98
Daniljela <daijela.blace@gmail.com>
2020-02-05 10:34:08 +0100

    main metod och GUI

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -1,5 +1,115 @@
 package gui;
 
-public class GUI {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+/**
+ * 
+ * @author danijela
+ *
+ */
+
+public class GUI extends JFrame implements ActionListener {
+	private JFrame frame = new JFrame("Kontaktbok");
+	private JPanel panel1 = new JPanel();
+	private JPanel panel2 = new JPanel();
+	private JPanel panel3 = new JPanel();
+	// private JLabel infoL = new JLabel("Skriv text i blå rutan och klicka den
+	// lämpliga knappen", JLabel.CENTER);
+	private JButton listaB = new JButton("Lista");
+	private JButton sökB = new JButton("Sök");
+	private JButton läggB = new JButton("Lägg");
+	private JButton taBortB = new JButton("Ta bort");
+	private JScrollPane sp;
+	// här man skriver text
+	private JTextField skrivInTF = new JTextField();
+
+	// Här man ser resultat efter mak klicka på knappen
+	private JTextArea resultatTA = new JTextArea(50, 50); // 20 rader, 1 kolumen
+
+	public GUI() {
+		skrivInTF.setBackground(Color.CYAN);
+		skrivInTF.setPreferredSize(new Dimension(200, 25));
+
+		resultatTA.setBackground(Color.GREEN);
+		// resultatTA.setPreferredSize(new Dimension(380,200));
+
+		panel1.setLayout(new FlowLayout(FlowLayout.CENTER));
+		panel1.setBackground(Color.GREEN);
+		panel2.setLayout(new FlowLayout(FlowLayout.CENTER));
+		panel2.setBackground(Color.GREEN);
+		panel3.setLayout(new FlowLayout(FlowLayout.CENTER));
+		panel3.setBackground(Color.GREEN);
+
+		// panel1.add(infoL);
+		panel1.add(skrivInTF);
+		panel2.add(listaB);
+		panel2.add(sökB);
+		panel2.add(läggB);
+		panel2.add(taBortB);
+
+		panel1.setBorder(new TitledBorder(new EtchedBorder(), "Skriv text i blå rutan"));
+		panel2.setBorder(new TitledBorder(new EtchedBorder(), "Klicka på den lämpliga knappen"));
+		panel3.setBorder(new TitledBorder(new EtchedBorder(), "Resultat"));
+
+		resultatTA.setEditable(false); // set textArea non-editable
+
+		// resultat ligger i JSCrollPane för att ha scroller
+		sp = new JScrollPane(resultatTA);
+		sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+		frame.add(panel1);
+		frame.add(panel2);
+		frame.add(sp);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(3, 1));
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+		listaB.addActionListener(this);
+		sökB.addActionListener(this);
+		läggB.addActionListener(this);
+		taBortB.addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == listaB) {
+			// metod1
+		}
+
+		if (e.getSource() == sökB) {
+			// metod2
+		}
+
+		if (e.getSource() == läggB) {
+			// metod3
+		}
+
+		if (e.getSource() == taBortB) {
+			// metod3
+		}
+
+	}
 
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,16 @@
 package main;
+import javax.swing.SwingUtilities;
+import gui.GUI;
 
+/**
+ * 
+ * @author danijela
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(() -> new GUI());
 
 	}
 
