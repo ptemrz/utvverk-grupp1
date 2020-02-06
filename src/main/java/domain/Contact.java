d142567c4ad2df9763b34c55aea45f2b677939e9
Peter Mrazek <56875892+ptemrz@users.noreply.github.com>
2020-02-06 14:28:28 +0100

    Add fields

--- src/main/java/domain/Contact.java
+++ src/main/java/domain/Contact.java
@@ -1,5 +1,18 @@
 package domain;
 
-public class Contact {
+import java.time.LocalDate;
 
+/**
+ * Contact POJO
+ * TODO getters, setters, toString, constructors
+ * @author ptemrz
+ *
+ */
+public class Contact {
+	private String title, firstName, middleName, lastName;
+//	private PhoneNumber[] numbers;
+//	private EmailAdress[] emails;
+	private String streetAdress, code, city, country;
+	private String comment;
+	private LocalDate birthday;
 }
