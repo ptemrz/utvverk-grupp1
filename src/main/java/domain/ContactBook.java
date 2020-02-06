907fff6c7d8c5c5477bd2b424c64c5db85f967fb
Peter Mrazek <56875892+ptemrz@users.noreply.github.com>
2020-02-06 14:17:08 +0100

    Implement the contact book as a linked list

--- src/main/java/domain/ContactBook.java
+++ src/main/java/domain/ContactBook.java
@@ -1,5 +1,15 @@
 package domain;
 
-public class ContactBook {
+import java.util.LinkedList;
 
+/**
+ * Right now a contact book is the same thing as a linked list of contacts
+ * TODO search algorithm
+ * @author ptemrz
+ *
+ */
+public class ContactBook extends LinkedList<Contact>{
+	private static final long serialVersionUID = 1L;
+	
+	
 }
