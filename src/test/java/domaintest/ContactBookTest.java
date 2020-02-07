ff2064c7b04e121fe40b527bc0bac96022728ca8
Peter Mrazek <56875892+ptemrz@users.noreply.github.com>
2020-02-07 10:06:46 +0100

    Test the find() method

--- src/test/java/domaintest/ContactBookTest.java
+++ src/test/java/domaintest/ContactBookTest.java
@@ -1,5 +1,35 @@
 package domaintest;
 
-public class ContactBook {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import domain.Contact;
+import domain.ContactBook;
+
+public class ContactBookTest {
+	private ContactBook cb;
+	
+	@BeforeEach
+	public void init() {
+		cb = new ContactBook();
+
+
+		
+		cb.add(new Contact("Adam"));
+		cb.add(new Contact("Bertil"));
+		cb.add(new Contact("Caesar"));
+		cb.add(new Contact("David"));
+	}
+	
+	@Test
+	public void testFind() {
+		List<Contact> found;
+		
+		found = cb.find("Adam");
+		assertEquals(1, found.size());
+	}
 }
