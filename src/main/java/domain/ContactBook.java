816729ee49f59646e97c39ac06a6ddd4d78fd04a
Peter Mrazek <56875892+ptemrz@users.noreply.github.com>
2020-02-07 09:45:27 +0100

    Add a search algorithm

--- src/main/java/domain/ContactBook.java
+++ src/main/java/domain/ContactBook.java
@@ -1,15 +1,36 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Right now a contact book is the same thing as a linked list of contacts
- * TODO search algorithm
+ * 
  * @author ptemrz
  *
  */
-public class ContactBook extends LinkedList<Contact>{
+public class ContactBook extends LinkedList<Contact> {
 	private static final long serialVersionUID = 1L;
+
 	
-	
+	/**
+	 * 
+	 * @param searchString the string to find
+	 * @return List of all contacts that match the searchString
+	 */
+	public List<Contact> find(String searchString) {
+		List<Contact> contacts = new ArrayList<>();
+
+		for (Contact c : this) {
+			for (String s : c.toStringArray()) {
+				if (s.equalsIgnoreCase(searchString) || s.contains(searchString)) {
+					contacts.add(c);
+					break;
+				}
+			}
+		}
+
+		return contacts;
+	}
 }
