0cc5aa02b1f402d8feb8cb07c20b0284d08cca51
Daniljela <daijela.blace@gmail.com>
2020-02-14 12:44:53 +0100

    Junit

--- src/test/java/gui/actionPerformedTest.java
+++ src/test/java/gui/actionPerformedTest.java
@@ -26,7 +26,7 @@ public class actionPerformedTest {
 	
 	/**
 	 * här testar vi funktionalitet av knappen "läggTill"
-	 * vi tillsätter text i JTextFiled och sen skickad det till JTextArea med hjälp av 
+	 * vi tillsätter text i JTextFiled-titel och sen skickad det till JTextArea med hjälp av 
 	 * knappar l"äggTill". Resultatet kan vi se med hjälp av knappen "lista"
 	 * Resultatet sätter vi i en string med hjälp av metoden getText() som hämtat textes som finns i
 	 * JTextArea. Denna resultat vi jämföra med en string( som innehåller (index: string och nya rad).
@@ -39,13 +39,13 @@ public class actionPerformedTest {
 		gui.getLäggB().doClick();
 		gui.getListaB().doClick();
 		String nyText = gui.getResultatTA().getText();
-		assertEquals("0: xxxx            \n", nyText);
+		assertEquals("1: xxxx            \n", nyText);
 	}
 
 	
 	/**
 	 * här testar vi funktionalitet av knappen "taBort"
-	 * Vi tillsätter text i JTextFiled och sen skickad det till JTextArea med hjälp av knappar l"äggTill" och  "lista"
+	 * Vi tillsätter text i JTextFiled-titel och sen skickad det till JTextArea med hjälp av knappar l"äggTill" och  "lista"
 	 *  Efter select vi det som vi innan skrev i JTextArea. Med hjälp av knappar taBort raderar vi kontakt från JTextArea.
 	 * Resultatet skrev vi i JTextArea med hjälp av "Lista" knappen . TExten som finns i JTextArea hämtar vi med hjälp av metoden getText().
 	 * Resultatet skulle vara en tomt string.
@@ -70,10 +70,10 @@ public class actionPerformedTest {
 	}
 	
 	/**
-	 * text som vi sätter i JTextFiled skickar vi till JTextArea med hjälp av knappar läggTill och lista. För det anvender vi metoder 
+	 * text som vi sätter i JTextFiled-titel skickar vi till JTextArea med hjälp av knappar läggTill och lista. För det använder vi metoden 
 	 * doClick() som simulerar en hendelse( att man klicka på knappen). Resultatet som finns som text i JTextArea ligger vi i en string variabel.
 	 * 
-	 * Nu sätter vi samma texten i JTextFiled en gån till och simulerar "click" av knappen "sök" med metoden doClick().
+	 * Nu sätter vi samma texten i JTextFiled-titel en gån till och simulerar "click" av knappen "sök" med metoden doClick().
 	 * Resultat sätter vi i andra string variabel. Vi jämföra dessa två stringer och m de är lika vår test passerar. 
 	 */
 
